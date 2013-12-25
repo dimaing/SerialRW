3869ecd29b441de4e9129f7fc6e9f9897f4bd5ca
Dmitry I <dimaing@hotmail.com>
2013-12-24 20:05:51 -0800

    missed one

---
+++ examples/scanf_printf_example/scanf_printf_example.ino
@@ -0,0 +1,25 @@
+#include <SerialRW.h>
+
+int x=0,y=0,z=0;
+int delayTime=10000;
+void setup(){
+  SerialRW::begin(9600); //configure Serial
+}
+void loop(){
+  non_blocking_scanf();
+  delay(delayTime);
+}
+/*
+Do not block execution if no Serial input
+ */
+void non_blocking_scanf(){
+  if(Serial.available()){
+    scanf ("%i %i %i",&x,&y,&z); //blocks execution until all expected values entered (3 integers in this case)
+    printf("You entered: %i, %i, %i \n", x, y, z);
+  }
+  else
+    printf("Nothing entered, are you ignoring me? Will wait for %d sec \n", delayTime/1000);  
+
+
+}
+
